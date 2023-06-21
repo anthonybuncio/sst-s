aed840c3d232452e685b5248f64444eb0b26368c
anthonybuncio <anthonyb.ft@gmail.com>
2023-06-21 02:02:12 -0500

    Deploy with actions

--- packages/functions/src/hello.ts
+++ packages/functions/src/hello.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { ApiHandler } from "sst/node/api";
 
+const DATA_URL = process.env.WEBHOOK_URL;
+
 export const handler = ApiHandler(async (evt) => {
 	// return {
 	// 	statusCode: 200,
@@ -9,8 +11,8 @@ export const handler = ApiHandler(async (evt) => {
 	// 	})
 	// };
 	try {
-		const response = await axios.post("https://discord.com/api/webhooks/1118083859783897159/DZ1CQonoT0evOe6hFOGQdSfnchxkxxFNC75MyxtQA1jQdx6-PUvNj3sZnk-DTiO4l3PH" + "?wait=true", {
-			content: `Hello, it's ${new Date().toLocaleTimeString('en-US')}. Here are some updates: `
+		const response = await axios.post(DATA_URL, {
+			content: `Hello, it's ${new Date().toLocaleString('en-US')}. Here are some updates: `
 		},
 			{
 				headers: {
--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,14 +1,19 @@
-import { StackContext, Api } from "sst/constructs";
+import { StackContext, Api, Cron } from "sst/constructs";
 
 export function API({ stack }: StackContext) {
-  const api = new Api(stack, "api", {
-    routes: {
-      "GET /": "packages/functions/src/hello.handler",
-    },
-
+  new Cron(stack, "Cron", {
+    schedule: "rate(5 minutes)",
+    job: "packages/functions/src/hello.handler",
   });
 
-  stack.addOutputs({
-    ApiEndpoint: api.url,
-  });
+  // const api = new Api(stack, "api", {
+  //   routes: {
+  //     "GET /": "packages/functions/src/hello.handler",
+  //   },
+
+  // });
+
+  // stack.addOutputs({
+  //   ApiEndpoint: api.url,
+  // });
 }
